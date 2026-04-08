97f4b0ead0a7246bea13e60f8e0d724f1ad27e29
YSH <yang5864@naver.com>
2026-04-08 16:15:45 +0900

    fix: vite.config.js import 중복 제거

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
